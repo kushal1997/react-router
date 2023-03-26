0814caf02d11c2a9a79c1653f347fcc5da68edb7
kushal1997 <kushalrao103@gmail.com>
2023-03-26 08:41:58 +0530

    route parameters

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,13 @@
-
+import { useSearchParams,useLocation } from "react-router-dom"
 
 export const ProductList = () => {
+  const [searchParams]=useSearchParams();
+  // console.log(searchParams.get("keyword"));
+  // console.log(searchParams.get("instock"));
+  // console.log(searchParams.get("rating"));
+  const location =useLocation();
+  console.log(location);
+  
   return (
     <div className="component">ProductList</div>
   )
